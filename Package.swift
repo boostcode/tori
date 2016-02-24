657ce9c885e3c1947da31321ff5b5df9ee3e17e2
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-02-24 23:41:20 +0100

    missing comma

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "tori"
+    name: "tori",
     
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,2,0)..<Version(0,3,0)),
