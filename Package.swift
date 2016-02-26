6bf5fe1fe5d31e046eb5f89f2c9e5ef2c8520c88
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-02-26 11:48:24 +0100

    stripped return

--- Package.swift
+++ Package.swift
@@ -18,7 +18,6 @@ import PackageDescription
 
 let package = Package(
     name: "tori",
-    
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,2,0)..<Version(0,3,0)),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", versions: Version(0,2,0)..<Version(0,3,0)),
