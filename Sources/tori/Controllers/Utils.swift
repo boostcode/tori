7390d9c2a474ab897615bf589bb1aa2a95140a6f
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-03-01 07:43:08 +0900

    working config file

--- Sources/tori/Controllers/Utils.swift
+++ Sources/tori/Controllers/Utils.swift
@@ -22,20 +22,37 @@ import SwiftyJSON
 // Retrieves tori configuration form config.json file
 func getConfiguration () -> (ConnectionProperties, String) {
     
+    let errorPrefix = "Configuration / "
+    
     guard let configDir = String.fromCString(getenv("CONFIG_DIR")) else {
-        print("Please set your CONFIG_DIR env var (export CONFIG_DIR=/path/)")
+        print(errorPrefix+"Please set your CONFIG_DIR env var (export CONFIG_DIR=/path/)")
         exit(1)
     }
     
     guard let configData = NSData(contentsOfFile: configDir + "./config.json") else {
-        print("Please check your config.json file exists at path "+configDir + "./config.json")
+        print(errorPrefix+"Please check your config.json file exists at path "+configDir + "./config.json")
         exit(1)    
     }
     
     let configJson = JSON(data: configData)
     
+    guard let dbHostName = configJson["couchDbIpAddress"].string else {
+        print(errorPrefix+"Missing ip address")
+        exit(1)
+    }
+    
+    guard let dbIpPort = configJson["couchDbPort"].number else{
+        print(errorPrefix+"Missing port address")
+        exit(1)
+    }
+    
+    guard let dbName = configJson["couchDbDbName"].number else {
+        print(errorPrefix+"Missing database name")
+        exit(1)
+    }
+
     return (
-        ConnectionProperties(hostName: "", port: Int16(27017), secured: false),
+        ConnectionProperties(hostName: dbHostName, port: Int16(dbIpPort), secured: false),
         "dbName"
     )
 
