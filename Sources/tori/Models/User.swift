388775b07db6454517942e703aa06d1df10119f9
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-03-02 12:52:08 +0900

    rollback code

--- Sources/tori/Models/User.swift
+++ Sources/tori/Models/User.swift
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
- 
+
  import CryptoSwift
- 
+
  struct User {
      // id
      var id: String = ""
