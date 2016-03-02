08fcc3e93c88f8576c997c1d1d9fe15930a5738d
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-03-02 11:42:16 +0900

    roll back to stable build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 rm -Rf .build Packages
-swift build -Xcc -fblocks -Xswiftc -I/usr/local/include -Xlinker -L/usr/local/lib && make
+#swift build -Xcc -fblocks -Xswiftc -I/usr/local/include -Xlinker -L/usr/local/lib --fetch && make
+swift build && make
