cadaedf1708f2d25f41bea3851dbcc9eb0938cb9
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-03-03 15:10:31 +0900

    improved db setup

--- Sources/tori/Controllers/Utils.swift
+++ Sources/tori/Controllers/Utils.swift
@@ -1,55 +1,98 @@
 /**
- * Copyright boostco.de 2016
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
+* Copyright boostco.de 2016
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
 
 import Foundation
 
-import CouchDB
 import SwiftyJSON
 
-// Retrieves tori configuration form config.json file
-func getConfiguration () -> (ConnectionProperties, String) {
-    
-    let errorPrefix = "Configuration / "
-  
-    guard let configDir = String.fromCString(getenv("TORI_CONFIG_DIR")) else {
-        print(errorPrefix+"Please set your CONFIG_DIR env var (e.g. export TORI_CONFIG_DIR=/var/tori/)")
-        exit(1)
-    }
-    guard let configData = NSData(contentsOfFile: configDir + "config.json") else {
-        print(errorPrefix+"Please check your config.json file exists at path "+configDir + "config.json")
-        exit(1)    
-    }
-    
-   let configJson = JSON(data: configData) 
-    guard let dbHostName = configJson["couchDbIpAddress"].string else {
-        print(errorPrefix+"Missing ip address")
-        exit(1)
-    }
-    guard let dbIpPort = configJson["couchDbPort"].number else{
-        print(errorPrefix+"Missing port address")
-        exit(1)
-    }
-    guard let dbName = configJson["couchDbDbName"].string else {
-        print(errorPrefix+"Missing database name")
-        exit(1)
-    }
+import Orca
+import OrcaSQLite
 
-    return (
-        ConnectionProperties(hostName: dbHostName, port: Int16(dbIpPort.intValue), secured: false),
-        dbName
-    )
+// database errors
+enum dbDriverError : ErrorType{
+  case MissingDriver
+  case NotSupportedYet
+  case MongoConnectionError
+}
+
+// database setup
+func setupDbDriver () throws -> Driver {
+  let (dbDriver, dbHost, dbPath, dbPort, dbName) = getDbConfiguration()
+
+  switch dbDriver {
+    case "SQLite":
+      return OrcaSQLite(path: dbPath+"/"+dbName)
+    case "MongoDB":
+      throw dbDriverError.NotSupportedYet
+      /*let mongo = OrcaMongoDB()
+      mongo.connect(host: dbHost, port: dbPort, database: dbName) { error in
+        if error {
+          throw dbDriverError.MongoConnectionError
+        }
+      }
+      return mongo*/
+    default:
+      throw dbDriverError.MissingDriver
+  }
+
+}
+
+// Retrieves tori db configuration form config.json file
+func getDbConfiguration () -> (String, String, String, UInt16, String) {
+
+  let errorPrefix = "Configuration / "
+
+  guard let configDir = String.fromCString(getenv("TORI_CONFIG_DIR")) else {
+    print(errorPrefix+"Please set your CONFIG_DIR env var (e.g. export TORI_CONFIG_DIR=/var/tori)")
+    exit(1)
+  }
+  guard let configData = NSData(contentsOfFile: configDir + "/config_db.json") else {
+    print(errorPrefix+"Please check your config.json file exists at path "+configDir + "/config_db.json")
+    exit(1)
+  }
+
+  let configJson = JSON(data: configData)
+
+  guard let dbDriver = configJson["dbDriver"].string else {
+    print(errorPrefix+"Missing db driver")
+    exit(1)
+  }
+  guard let dbHostname = configJson["dbHost"].string else {
+    print(errorPrefix+"Missing ip address")
+    exit(1)
+  }
+  guard let dbIpPort = configJson["dbPort"].number else{
+    print(errorPrefix+"Missing port address")
+    exit(1)
+  }
+  guard let dbName = configJson["dbName"].string else {
+    print(errorPrefix+"Missing database name")
+    exit(1)
+  }
+  guard let dbPath = configJson["dbPath"].string else {
+    print(errorPrefix+"Missing database path")
+    exit(1)
+  }
+
+  return (
+    dbDriver,
+    dbHostname,
+    dbPath,
+    UInt16(dbIpPort.intValue),
+    dbName
+  )
 
 }
--- Sources/tori/main.swift
+++ Sources/tori/main.swift
@@ -1,28 +1,30 @@
 /**
- * Copyright boostco.de 2016
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
- 
+* Copyright boostco.de 2016
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
 import Foundation
- 
+
 // kitura
 import KituraRouter
 import KituraNet
 import KituraSys
 
-// couchdb
-import CouchDB
+// db orm
+import Orca
+import OrcaSQLite
+//import OrcaMongoDB
 
 // logger
 import HeliumLogger
@@ -34,15 +36,17 @@ let router = Router()
 // setup logger
 Log.logger = HeliumLogger()
 
-// retrieve tori configuration
-let (mongoConnection, mongoDbName) = getConfiguration()
+// setup database connection
+let dbDriver = try! setupDbDriver()
+let db = Orca(driver: dbDriver)
 
+// routing
 router.get("/") {
-request, response, next in
+  request, response, next in
 
-    response.status(HttpStatusCode.OK).send("Hello, tori!")
+  response.status(HttpStatusCode.OK).send("Hello, tori!")
 
-    next()
+  next()
 }
 
 // setup server
