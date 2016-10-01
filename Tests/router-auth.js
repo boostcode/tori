07d190a8d4e1ccacf02a355081f828724e06764d
Matteo Crippa <matteo@boostco.de>
2016-10-01 17:16:45 +0200

    added tests (in nodejs) for routes

---
+++ Tests/router-auth.js
@@ -0,0 +1,32 @@
+var supertest = require('supertest'),
+    should = require('chai').should(),
+    expect = require('chai').expect,
+    mongoClient = require('mongodb').MongoClient;
+
+var api = supertest('http://localhost:8090');
+
+var adminUserName = "admin";
+var adminUserPassword = "admin";
+
+describe('Authentication', function(){
+         
+         it('can login admin', function(done){
+            
+            api.post('/api/login')
+            .type('json')
+            .send({
+                  'username': adminUserName,
+                  'password': adminUserPassword
+                  })
+            .expect(200)
+            .end(function(err, res){
+                 expect(res.body).to.not.equal(null);
+                 expect(res.body.status).to.not.equal("error");
+                 expect(res.body.status).to.equal("ok");
+                 expect(res.body.user).to.equal("admin");
+                 expect(res.body.token).to.not.equal(null);
+                 done();
+            });
+        });
+
+});
